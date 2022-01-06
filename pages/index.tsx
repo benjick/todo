6af187372ee77060b23c3079b4fdf964d0e7c265
Max Malm <benjick@dumfan.net>
2022-01-06 23:50:20 +0100

    Create title component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,22 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import { useTodo } from "../src/state";
+import { DerivedCategory, useTodo } from "../src/state";
 import { AddCategory } from "../components/AddCategory";
 import { ShowCompleted } from "../components/ShowCompleted";
 import { AddItem } from "../components/AddItem";
 import { TodoItem } from "../components/TodoItem";
 import { EmptyState } from "../components/EmptyState";
+
+const CategoryTitle: React.FC<{ category: DerivedCategory }> = ({
+  category,
+}) => (
+  <p className="block text-sm font-medium text-gray-700 mb-4">
+    {category.name}{" "}
+    {category.closeAfterFinished ? (
+      <span>(complete {category.closeAfterFinished})</span>
+    ) : undefined}
+  </p>
+);
 
 const Home: NextPage = () => {
   const { categoriesWithItems, showCompleted } = useTodo();
@@ -20,24 +31,18 @@ const Home: NextPage = () => {
       </Head>
 
       <main>
-        <span className="relative z-0 inline-flex shadow-sm rounded-md m-2">
+        <div className="relative z-0 inline-flex shadow-sm rounded-md m-2">
           <AddItem />
           <AddCategory />
           <ShowCompleted />
-        </span>
-        <hr />
+        </div>
 
         {categories.length > 0 ? (
           <div className="bg-white shadow overflow-hidden sm:rounded-md">
             <ul role="list" className="divide-y divide-gray-200">
               {categories.map((category) => (
                 <li key={category.id} className="px-4 py-4 sm:px-6">
-                  <p className="block text-sm font-medium text-gray-700 mb-4">
-                    {category.name}{" "}
-                    {category.closeAfterFinished ? (
-                      <span>(complete {category.closeAfterFinished})</span>
-                    ) : undefined}
-                  </p>
+                  <CategoryTitle category={category} />
                   <ul role="list" className="-my-5 divide-y divide-gray-200">
                     {category.items
                       .filter((item) => showCompleted || !item.done)
