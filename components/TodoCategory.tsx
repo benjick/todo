c529ca0bf07c8601b282761fe988bf857f9841b3
Max Malm <benjick@dumfan.net>
2022-01-07 01:55:22 +0100

    Create category component

---
+++ components/TodoCategory.tsx
@@ -0,0 +1,43 @@
+import { DerivedCategory } from "../src/state";
+import { TodoItem } from "../components/TodoItem";
+import { useCategoryForm } from "../src/form/category";
+
+const Title: React.FC<{ category: DerivedCategory }> = ({ category }) => {
+  const { setOpen, setForm } = useCategoryForm();
+  return (
+    <button
+      onClick={() => {
+        setForm(category.id, {
+          name: category.name,
+          closeAfterFinished: category.closeAfterFinished,
+          resetAfterDays: category.resetAfterDays,
+        });
+        setOpen(true);
+      }}
+      className="block text-sm font-medium text-gray-700 mb-4"
+    >
+      {category.name}{" "}
+      {category.closeAfterFinished ? (
+        <span>(complete {category.closeAfterFinished})</span>
+      ) : undefined}
+    </button>
+  );
+};
+
+export const TodoCategory: React.FC<{
+  category: DerivedCategory;
+  showCompleted: boolean;
+}> = ({ category, showCompleted }) => {
+  return (
+    <li key={category.id} className="px-4 py-4 sm:px-6">
+      <Title category={category} />
+      <ul role="list" className="-my-5 divide-y divide-gray-200">
+        {category.items
+          .filter((item) => showCompleted || !item.done)
+          .map((item) => (
+            <TodoItem key={item.id} item={item} />
+          ))}
+      </ul>
+    </li>
+  );
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,38 +1,13 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import { DerivedCategory, useTodo } from "../src/state";
+import { useTodo } from "../src/state";
 import { AddCategory } from "../components/AddCategory";
 import { ShowCompleted } from "../components/ShowCompleted";
 import { AddItem } from "../components/AddItem";
-import { TodoItem } from "../components/TodoItem";
 import { EmptyState } from "../components/EmptyState";
 import { CategoryPopup } from "../components/CategoryPopup";
-import { useCategoryForm } from "../src/form/category";
 import { ItemPopup } from "../components/ItemPopup";
-
-const CategoryTitle: React.FC<{ category: DerivedCategory }> = ({
-  category,
-}) => {
-  const { setOpen, setForm } = useCategoryForm();
-  return (
-    <button
-      onClick={() => {
-        setForm(category.id, {
-          name: category.name,
-          closeAfterFinished: category.closeAfterFinished,
-          resetAfterDays: category.resetAfterDays,
-        });
-        setOpen(true);
-      }}
-      className="block text-sm font-medium text-gray-700 mb-4"
-    >
-      {category.name}{" "}
-      {category.closeAfterFinished ? (
-        <span>(complete {category.closeAfterFinished})</span>
-      ) : undefined}
-    </button>
-  );
-};
+import { TodoCategory } from "../components/TodoCategory";
 
 const Home: NextPage = () => {
   const { categoriesWithItems, showCompleted } = useTodo();
@@ -61,16 +36,11 @@ const Home: NextPage = () => {
           <div className="bg-white shadow overflow-hidden sm:rounded-md">
             <ul role="list" className="divide-y divide-gray-200">
               {categories.map((category) => (
-                <li key={category.id} className="px-4 py-4 sm:px-6">
-                  <CategoryTitle category={category} />
-                  <ul role="list" className="-my-5 divide-y divide-gray-200">
-                    {category.items
-                      .filter((item) => showCompleted || !item.done)
-                      .map((item) => (
-                        <TodoItem key={item.id} item={item} />
-                      ))}
-                  </ul>
-                </li>
+                <TodoCategory
+                  key={category.id}
+                  category={category}
+                  showCompleted={showCompleted}
+                />
               ))}
             </ul>
           </div>
