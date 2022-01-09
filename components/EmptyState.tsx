04095158e84f24e7d323dc4ccd804f8a438724c5
Max Malm <benjick@dumfan.net>
2022-01-09 18:38:11 +0100

    empty state padding

--- components/EmptyState.tsx
+++ components/EmptyState.tsx
@@ -2,7 +2,7 @@ import { EmojiHappyIcon } from "@heroicons/react/outline";
 
 export const EmptyState: React.FC = () => {
   return (
-    <div className="text-center mt-12">
+    <div className="text-center mt-12 pb-4 px-4">
       <EmojiHappyIcon className="mx-auto h-12 w-12 text-gray-400" />
       <h3 className="mt-2 text-sm font-medium text-gray-900">No more tasks!</h3>
       <p className="mt-1 text-sm text-gray-500">
