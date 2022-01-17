1af2a4050d6beee352efc54576f5f509206006a1
Max Malm <benjick@dumfan.net>
2022-01-17 12:53:09 +0100

    Use dates for timer

--- components/TodoItem.tsx
+++ components/TodoItem.tsx
@@ -7,98 +7,100 @@ import { msToTime } from "../src/helpers";
 import useSound from "use-sound";
 import lofiBells from "../assets/lofi-bells.mp3";
 import { useItemForm } from "../src/form/item";
+import { default as dayjs } from "dayjs";
 
-const Child: React.FC<{ title: string; onClick: (e: any) => void; icon: any }> =
-  ({ children, title, onClick, icon: Icon }) => {
-    return (
-      <Transition.Child
-        as={Fragment}
-        enter="ease-out duration-300"
-        enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-        enterTo="opacity-100 translate-y-0 sm:scale-100"
-        leave="ease-in duration-200"
-        leaveFrom="opacity-100 translate-y-0 sm:scale-100"
-        leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-      >
-        <div className="w-full inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-sm sm:w-full sm:p-6">
-          <div>
-            <div className="mx-auto flex items-center justify-center h-12 w-12 rounded-full bg-green-100">
-              <Icon className="h-6 w-6 text-green-600" aria-hidden="true" />
-            </div>
-            <div className="mt-3 text-center sm:mt-5">
-              <Dialog.Title
-                as="h3"
-                className="text-lg leading-6 font-medium text-gray-900"
-              >
-                {title}
-              </Dialog.Title>
-              <div className="mt-2">
-                <p className="text-sm text-gray-500">{children}</p>
-              </div>
+const Child: React.FC<{
+  title: string;
+  onClick: (e: any) => void;
+  icon: any;
+}> = ({ children, title, onClick, icon: Icon }) => {
+  return (
+    <Transition.Child
+      as={Fragment}
+      enter="ease-out duration-300"
+      enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+      enterTo="opacity-100 translate-y-0 sm:scale-100"
+      leave="ease-in duration-200"
+      leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+      leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+    >
+      <div className="w-full inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-sm sm:w-full sm:p-6">
+        <div>
+          <div className="mx-auto flex items-center justify-center h-12 w-12 rounded-full bg-green-100">
+            <Icon className="h-6 w-6 text-green-600" aria-hidden="true" />
+          </div>
+          <div className="mt-3 text-center sm:mt-5">
+            <Dialog.Title
+              as="h3"
+              className="text-lg leading-6 font-medium text-gray-900"
+            >
+              {title}
+            </Dialog.Title>
+            <div className="mt-2">
+              <p className="text-sm text-gray-500">{children}</p>
             </div>
           </div>
-          {onClick && (
-            <div className="mt-5 sm:mt-6">
-              <button
-                type="button"
-                className="inline-flex justify-center w-full rounded-md border border-transparent shadow-sm px-4 py-2 bg-indigo-600 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:text-sm"
-                onClick={onClick}
-              >
-                I&apos;m done!
-              </button>
-            </div>
-          )}
         </div>
-      </Transition.Child>
-    );
-  };
+        {onClick && (
+          <div className="mt-5 sm:mt-6">
+            <button
+              type="button"
+              className="inline-flex justify-center w-full rounded-md border border-transparent shadow-sm px-4 py-2 bg-indigo-600 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:text-sm"
+              onClick={onClick}
+            >
+              I&apos;m done!
+            </button>
+          </div>
+        )}
+      </div>
+    </Transition.Child>
+  );
+};
 
 const Popup: React.FC<{
-  showTimer: boolean;
-  setShowTimer: (v: boolean) => void;
+  open: boolean;
+  setOpen: (v: boolean) => void;
   finishedTodo: () => void;
   minutes: number;
-}> = ({ showTimer, setShowTimer, finishedTodo, minutes }) => {
-  const [timeleft, setTimeleft] = useState(() => minutes * 60 * 1000);
+}> = ({ open, setOpen, finishedTodo, minutes }) => {
+  const [endTime, setEndTime] = useState<Date>();
+  const [currentTime, setCurrentTime] = useState<Date>();
+
+  const hasDates = endTime && currentTime;
+  const timeleft = dayjs(endTime).diff(currentTime);
 
   const [play, { stop }] = useSound(lofiBells);
   const intervalRef = useRef<NodeJS.Timer>();
   const ms = 100;
 
-  // Start the timer
+  // Opening and closing the popup
   useEffect(() => {
-    // TODO: Maybe redo this to something better compares to the opened time instead
-    if (showTimer) {
+    if (open) {
+      setEndTime(dayjs().add(minutes, "minutes").toDate());
       intervalRef.current = setInterval(() => {
-        setTimeleft((timeleft) => timeleft - ms);
+        setCurrentTime(new Date());
       }, ms);
+    } else {
+      stop();
+      intervalRef.current && clearInterval(intervalRef.current);
     }
-  }, [showTimer, play]);
+  }, [open, stop, minutes]);
 
   // End the timer when time = 0
   useEffect(() => {
-    if (showTimer && intervalRef.current && timeleft < 1) {
+    if (open && hasDates && intervalRef.current && timeleft < 1) {
       play();
       clearInterval(intervalRef.current);
       intervalRef.current = undefined;
     }
-  }, [timeleft, play, showTimer]);
-
-  // When closing the popup
-  useEffect(() => {
-    if (!showTimer) {
-      stop();
-      setTimeleft(minutes * 60 * 1000);
-      intervalRef.current && clearInterval(intervalRef.current);
-    }
-  }, [showTimer, stop, minutes]);
+  }, [timeleft, play, open, hasDates]);
 
   return (
-    <Transition.Root show={showTimer} as={Fragment}>
+    <Transition.Root show={open} as={Fragment}>
       <Dialog
         as="div"
         className="fixed z-10 inset-0 overflow-y-auto"
-        onClose={setShowTimer}
+        onClose={setOpen}
       >
         <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
           <Transition.Child
@@ -160,8 +162,8 @@ export const TodoItem: React.FC<{ item: DerivedItem }> = ({ item }) => {
     <li className="py-4">
       {item.timerMinutes ? (
         <Popup
-          showTimer={showTimer}
-          setShowTimer={setShowTimer}
+          open={showTimer}
+          setOpen={setShowTimer}
           finishedTodo={finishedTodo}
           minutes={item.timerMinutes}
         />
